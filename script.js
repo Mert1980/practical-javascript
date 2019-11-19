665ecf0f4672c7fac5c3f619a97fa6ea85d6cd04
Mert Demirok <mertdemirok80@gmail.com>
2019-11-19 22:40:11 +0100

    functions changed to methods in an object

--- script.js
+++ script.js
@@ -1,65 +1,26 @@
-// VERSION-01 ARRAYS
-//Store todos
-var todos = ["item 1", "item 2", "item 3"];
+// VERSION-3 OBJECTS
+// REQUIREMENTS FOR VERSION3
+// 1-IT SHOULD STORE THE TODOS ARRAY ON AN OBJECT
+// 2-IT SHOULD HAVE A DISPLAYTODOS METHOD
+// 3-IT SHOULD HAVE AN ADDTODO METHOD
+// 4-IT SHOULD HAVE A CHANGETODO METHOD
+// 5-IT SHOULD HAVE A DELETETODO METHOD
 
-//Display todos
-console.log("My Todos:", todos);
-
-//Add new todos
-todos.push("item 4");
-todos.push("item 5");
-console.log("My Todos after 2 times Push:", todos);
-
-//Change item in existing todos
-todos[0] = "item 1 updated";
-console.log("My Todos after first item updated:", todos);
-
-//Delete item in existing todos
-todos.splice(0, 1); //from first item(index) one item delete.
-console.log("My Todos; first item deleted:", todos);
-todos.splice(3, 1); //from third item(index) one item delete.
-console.log("My Todos; last item deleted:", todos);
-
-// Examples for Array
-
-//Add item as first item to todos.
-var unshiftedItem = todos.unshift("item 1");
-console.log("My Todos; first item unshifted again:", todos);
-console.log("unShifted item:", todos[0]);
-
-//Remove first item from todos.
-var shiftedItem = todos.shift();
-console.log("My Todos; first item deleted with shift:", todos);
-console.log("Shifted item:", shiftedItem);
-
-// VERSION-2 FUNCTIONS 
-// REQUIREMENTS
-// 1-IT SHOULD HAVE A FUNCTION TO DISPLAY TODOS
-// 2-IT SHOULD HAVE A FUNCTION TO ADD NEW TODOS
-// 3-IT SHOULD HAVE A FUNCTION TO CHANGE A TODO
-// 4-IT SHOULD HAVE A FUNCTION TO DELETE A TODO
-
-var todos = ['item1', 'item2', 'item3']; // global var which we can acces from any function
-    
-   function displayTodos () {
-        console.log('my todos:', todos);
-   }
-    displayTodos();
-   
-   function addTodo(todo){
-       todos.push(todo);
-       displayTodos();
-   }
-   addTodo('item4'); 
-   
-   function changeTodo(index,value){
-       todos[index] = value;
-       displayTodos();
-   }
-   changeTodo(1,'changed');
-
-   function deleteTodo(index){
-       todos.splice(index,1);
-       displayTodos();
-   }
-   deleteTodo(2);
+   var todolist = {
+    todos: ['item1', 'item2', 'item3'],
+    displayTodos: function(){
+        console.log('here:',this.todos)},
+    addTodo: function (a){
+        this.todos.push(a);
+        this.displayTodos();},
+        //todolist.addTo('added item')
+    changeTodo: function (index, value){
+        this.todos[index] = value;
+        this.displayTodos();},
+        //todolist.changeTo(0,'changed')
+    deleteTodo: function(index){
+        this.todos.splice(index,1);
+        this.displayTodos();
+        //tolist.deleteTo(0)
+    }
+    };
