6102dcc70aa59588d4fa3177ed8385d6162dce9b
Mert Demirok <mertdemirok80@gmail.com>
2019-11-19 23:26:46 +0100

    version5 completed

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// VERSION-4 BOOLEANS
-// REQUIREMENTS FOR VERSION4
-// 1-todoList.AddTodo should add objects
-// 2-todoList.ChangeTodo should change the todoText property
-// 3-todoList.toggleCompleted should change the completed property
+// VERSION-5 LOOPS OF LOGIC
+// REQUIREMENTS FOR VERSION5
+// 1- .displayTodos should show .todoText
+// 2- .displayTodos should tell you if .todos empty
+// 3- .displayTodos should show .completed
 
 var todolist = {
     todos: [],
     displayTodos: function(){
-        console.log('here:',this.todos)},
+      if (this.todos.length === 0){
+          console.log('Your todo list is empty!')
+      } else {
+        console.log('My Todos:')
+        for (var i=0; i<this.todos.length; i++){
+             if (this.todos[i].completed === true){
+                 console.log('(X)',this.todos[i].todoText);
+             } else {
+                console.log('( )',this.todos[i].todoText);
+            }
+          }
+        }
+    },
     addTodo: function (todoText){
         this.todos.push({
             todoText:todoText,
